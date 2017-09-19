154f90659a425e07b77b5527cc34491c66b9a693
Matt Lee <matthewlee07@gmail.com>
2017-09-19 17:31:48 -0400

    get put post working

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ function require = {
 }
 */
 const { PORT, DATABASE_URL } = require('./config');
-const {BlogPost} = require('./models');
+const { BlogPost } = require('./models');
 
 const app = express();
 app.use(bodyParser.json());
@@ -19,77 +19,122 @@ app.use(bodyParser.json());
 
 app.get('/posts', (req, res) => {
   BlogPost
-  .find()
-  .limit(20)
-  .then(blogPosts => {
-    res.json({
-      blogPosts: blogPosts.map(
-        (blogPost) => blogPost.apiRepr()
-      )
-    });
-  })
-  .catch(
-    err =>{
+    .find()
+    .limit(20)
+    .then(blogPosts => {
+      res.json({
+        blogPosts: blogPosts.map(
+          (blogPost) => blogPost.apiRepr()
+        )
+      });
+    })
+    .catch(
+    err => {
       console.error(err);
-      res.sendStatus(500);    
+      res.sendStatus(500);
     });
 });
 
 app.get('/posts/:id', (req, res) => {
-  res.sendStatus(500);
+  BlogPost
+    .findById(req.params.id)
+    .then(blogPosts => res.json(blogPosts.apiRepr()))
+    .catch(
+    err => {
+      console.error(err);
+      res.sendStatus(500);
+    });
 })
 
-app.post('/posts/', (req, res) => {
-  res.sendStatus(500);
-})
+app.post('/posts', (req, res) => {
+  const requiredFields = ['title', 'content', 'author'];
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \` ${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  BlogPost
+    .create({
+      title: req.body.title,
+      content: req.body.content,
+      author: req.body.author,
+    })
+    .then(
+    blogPost => res.status(200).json(blogPost.apiRepr()))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: 'Internal server error' });
+    });
+});
 
 app.put('/posts/:id', (req, res) => {
-  res.sendStatus(500);
-})
-
-app.delete('/posts/:id', (req, res) => {
-  res.sendStatus(500);
-})
-
-let server;
-
-function runServer(databaseUrl = DATABASE_URL, port = PORT) {
-
-  return new Promise((resolve, reject) => {
-    mongoose.connect(databaseUrl, err => {
-      if (err) {
-        return reject(err);
-      }
-      server = app.listen(port, () => {
-        console.log(`Your app is listening on port ${port}`);
-        resolve();
-      })
-        .on('error', err => {
-          mongoose.disconnect();
-          reject(err);
-        });
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    res.status(400).json({
+      error: 'Request path id and request body id values must match'
     });
+  }
+
+  const updated = {};
+  const updateableFields = ['title', 'content', 'author'];
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      updated[field] = req.body[field];
+    }
   });
-}
 
-function closeServer() {
-  return mongoose.disconnect().then(() => {
-     return new Promise((resolve, reject) => {
-       console.log('Closing server');
-       server.close(err => {
-           if (err) {
-               return reject(err);
-           }
-           resolve();
-       });
-     });
-  });
-}
+  BlogPost
+    .findByIdAndUpdate(req.params.id, {$set: updated}, {new: true})
+    .then(updatedPost => res.status(204).end())
+    .catch(err => res.status(500).json({message: 'Something went wrong'}));
+});
 
-if (require.main === module) {
-  runServer().catch(err => console.error(err));
-};
+// app.delete('/posts/:id', (req, res) => {
+//   res.sendStatus(500);
+// })
 
-module.exports = {app, runServer, closeServer};
+  let server;
 
-console.log(DATABASE_URL);
+  function runServer(databaseUrl = DATABASE_URL, port = PORT) {
+
+    return new Promise((resolve, reject) => {
+      mongoose.connect(databaseUrl, err => {
+        if (err) {
+          return reject(err);
+        }
+        server = app.listen(port, () => {
+          console.log(`Your app is listening on port ${port}`);
+          resolve();
+        })
+          .on('error', err => {
+            mongoose.disconnect();
+            reject(err);
+          });
+      });
+    });
+  }
+
+  function closeServer() {
+    return mongoose.disconnect().then(() => {
+      return new Promise((resolve, reject) => {
+        console.log('Closing server');
+        server.close(err => {
+          if (err) {
+            return reject(err);
+          }
+          resolve();
+        });
+      });
+    });
+  }
+
+  if (require.main === module) {
+    runServer().catch(err => console.error(err));
+  };
+
+  module.exports = { app, runServer, closeServer };
+
+  console.log(DATABASE_URL);
